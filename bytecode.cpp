701ed58b3737f15e1df11ddc8f0bb7363075fb58
Dario Cangialosi <dario.cangialosi@gmail.com>
2016-12-30 22:05:15 +0100

    3 new functions (see main function comment)

--- bytecode.cpp
+++ bytecode.cpp
@@ -32,6 +32,116 @@ void runByteCode(byte_t* byteCode){
 	}
 }
 
+void runByteCode2(){
+//byte_t* byteCode
+	//byte_t progNot[]={0xFF, 0xFE, 0x02, 0x01, 0xFE, 0x00, 0x00, 0x00, 0xFE, 0x01, 0x00, 0x00};
+
+	/*
+	#not_gate.source
+	pch=in
+	zero|one
+	@zero
+	out=0
+	0
+	@one
+	out=1
+	0
+	*/
+	
+	struct instruction{
+		unsigned int type:1;
+		unsigned int index:7;
+	};
+	struct instruction byteCodeProgram[]={ {0,0},{1,0}, {0,1},{1,1}, {0,2},{1,2}};
+	// 0:MemCopy 1:PathChoice
+	byte_t byteCodeMemCopy[]={255,254, 254,0, 254,1};
+	byte_t byteCodePathChoice[]={4,2, 0,0, 0,0};
+	
+	bitset<256> memory;
+	memory[1]=1;
+	size_t instructionPointer=0;
+	
+	while(instructionPointer!=255){
+		struct instruction instr=byteCodeProgram[instructionPointer];
+		if(false){
+			cout<<"instructionPointer:"<<instructionPointer<<endl;
+			cout<<"instr.type:"<<instr.type<<endl;
+			cout<<"instr.index:"<<instr.index<<endl;
+			cout<<"memory[255]:"<<memory[255]<<endl;
+		}
+		if(instr.type==0){
+			byte_t in=byteCodeMemCopy[instr.index*2+1]; int x;
+			
+			if(in==254) x=readBit();
+			else x=memory[in];
+			
+			byte_t out=byteCodeMemCopy[instr.index*2];
+			
+			if(out==254) cout<<"out:"<<x<<endl;
+			else memory[out]=x;
+
+			instructionPointer++;
+			
+		} else if(instr.type==1){
+			instructionPointer=byteCodePathChoice[instr.index*2+memory[255]];
+		}
+	}
+}
+
+void runByteCode3(byte_t byteCode[]){
+//byte_t* byteCode
+	//byte_t progNot[]={0xFF, 0xFE, 0x02, 0x01, 0xFE, 0x00, 0x00, 0x00, 0xFE, 0x01, 0x00, 0x00};
+
+	/*
+	#not_gate.source
+	pch=in
+	zero|one
+	@zero
+	out=0
+	0
+	@one
+	out=1
+	0
+	*/
+	
+	// 0:MemCopy 1:PathChoice
+	//byte_t byteCode[]={0,255,254, 1,4,2, 0,254,0, 1,0,0, 0,254,1, 1,0,0};
+	
+	bitset<256> memory;
+	memory[1]=1;
+	size_t instructionPointer=0;
+	
+	while(instructionPointer!=255){
+		byte_t type=byteCode[instructionPointer*3+0];
+
+		if(false){
+			cout<<"instructionPointer:"<<instructionPointer<<endl;
+			cout<<"instr.type:"<<type<<endl;
+			cout<<"memory[255]:"<<memory[255]<<endl;
+		}
+		if(type==0){
+
+			byte_t first=byteCode[instructionPointer*3+1];
+			byte_t second=byteCode[instructionPointer*3+2];
+			
+			byte_t in=second; int x;
+			
+			if(in==254) x=readBit();
+			else x=memory[in];
+			
+			byte_t out=first;
+			
+			if(out==254) cout<<"out:"<<x<<endl;
+			else memory[out]=x;
+
+			instructionPointer++;
+			
+		} else if(type==1){
+			instructionPointer=byteCode[instructionPointer*3+1+memory[255]];
+		}
+	}
+}
+
 byte_t* readBytes(const char *name){
 	ifstream fl(name);
 	fl.seekg( 0, ios::end );
@@ -76,7 +186,25 @@ void byteCodeFromProgNotArray2(){
 	delete[] progNotArray;
 }
 
+void byteCodeFromProgNotArray3(){
+	byte_t array[]={0,255,254, 1,4,2, 0,254,0, 1,0,0, 0,254,1, 1,0,0};
+	runByteCode3(array);
+}
+
+void byteCodeFromProgNotArray4(){
+	//byte_t array[]={0,255,254, 1,4,2, 0,254,0, 1,0,0, 0,254,1, 1,0,0};
+	string progNotString="00FFFE01040200FE0001000000FE01010000";
+	byte_t* progNotArray = byteArrayFromHexString(progNotString);
+	runByteCode3(progNotArray);
+	delete[] progNotArray;
+}
+
 int main(int argc, char* argv[]){
 	//byteCodeFromArgVFile(argc, argv);
-	byteCodeFromProgNotArray2();
+	//byteCodeFromProgNotArray2();
+
+	// added new
+	//runByteCode2();
+	//byteCodeFromProgNotArray3();
+	byteCodeFromProgNotArray4();
 }
